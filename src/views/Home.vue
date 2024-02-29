74002e4b26779641051e88d596514d54e63e3a2d
christosad <37677082+christosad@users.noreply.github.com>
2024-02-29 20:31:04 +0200

    adding images on build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,6 +25,10 @@
 import MainButton from '../components/MainButton.vue'
 import BubbleImages from '../components/BubbleImages.vue'
 import Arrow from '../components/Arrow.vue'
+import image1 from '../assets/image1.jpg'
+import image2 from '../assets/image2.jpg'
+import image3 from '../assets/image3.jpg'
+import image4 from '../assets/image4.jpg'
 </script>
 
 <style scoped>
